378a5e4695ab05722a1c4e9563fdcffefa6c6c42
Nikhil Singh <nikhilbangalor@gmail.com>
2021-03-29 18:11:08 +0530

    Add files via upload

---
+++ Stack/Implementing Stack using Linked List.py
@@ -0,0 +1,46 @@
+# Implementing Stack using Linked List:
+
+
+class StackNode:
+
+    # Constructor to initialize a node
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Stack:
+    # Constructor to initialize the root of linked list
+    def __init__(self):
+        self.root = None
+
+    def isEmpty(self):
+        return True if self.root is None else False
+
+    def push(self, data):
+        newNode = StackNode(data)
+        newNode.next = self.root
+        self.root = newNode
+        print("%d pushed to stack" % data)
+
+    def pop(self):
+        if self.isEmpty():
+            return "Stack Underflow"
+        temp = self.root
+        self.root = self.root.next
+        popped = temp.data
+        return popped
+
+    def peek(self):
+        if self.isEmpty():
+            return "Stack Underflow"
+        return self.root.data
+
+
+stack = Stack()
+stack.push(10)
+stack.push(20)
+stack.push(30)
+
+print("%d popped from stack" % (stack.pop()))
+print("Top element in the stack is %d" % (stack.peek()))
